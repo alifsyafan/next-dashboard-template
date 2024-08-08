438ac78c6066bc5c3010578e5abc5323b99b50f7
alifsyafan <105841114422@student.unismuh.ac.id>
2024-08-08 16:04:02 +0800

    chapter 3 selesai

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,12 @@
 // /app/layout.js
 
 import "@/app/ui/global.css";
-
-export default function RootLayout({ children }) {
+import { lusitana } from "./ui/fonts";
+export default function RootLayout({ children }) 
+{
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
+      </html>
   );
 }
