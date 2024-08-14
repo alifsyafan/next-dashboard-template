4965e426246167135f91a848bec7b9b3381064c2
alifsyafan <105841114422@student.unismuh.ac.id>
2024-08-14 22:44:17 +0800

    chapter 3 selesai

--- app/page.js
+++ app/page.js
@@ -6,9 +6,8 @@ import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
-        {/* ... */}
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-700 p-4 md:h-52">
+        {<AcmeLogo />}
       </div>
       <div className="flex flex-col gap-4 mt-4 grow md:flex-row">
         <div className="flex flex-col justify-center gap-6 px-6 py-10 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
@@ -21,18 +20,26 @@ export default function Page() {
           </p>
           <Link
             href="/login"
-            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-blue-500 rounded-lg hover:bg-blue-400 md:text-base"
+            className="flex items-center self-start gap-5 px-6 py-3 text-sm font-medium text-white transition-colors bg-blue-700 rounded-lg hover:bg-red-700 md:text-base"
           >
             <span>Log in</span>
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image
+          {/* Add Hero Images Here */}
+          <img
             src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <img
+            src="/hero-mobile.png"
             width={560}
             height={620}
             className="block md:hidden"
-            alt="Gambar untuk mobile"
+            alt="Screenshots of the dashboard project showing desktop version"
           />
         </div>
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
